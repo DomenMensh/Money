28b693391bb96fe625ff418bf60afd3a2377d84b
DomenMensh <d_mensh@mail.ru>
2021-09-11 10:38:15 +0500

    Добавьте файлы проекта.

---
+++ 04.09.21 Классы и перегрузка/Money.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04._09._21_Классы_и_перегрузка
+{
+    class Money
+    {
+        private int rubles;
+        private int kopeks;
+
+        public int Rubles
+        {
+            get
+            {
+                return rubles;
+            }
+
+            set
+            {
+                rubles = value;
+            }
+        }
+
+        public int Kopeks
+        {
+            get
+            {
+                return kopeks;
+            }
+
+            set
+            {
+                kopeks = value;
+            }
+        }
+
+        public void AddKopeks(int k)
+        {
+            int summa = kopeks + k;
+            int r = summa / 100;
+            if (r > 0)
+            {
+                rubles += r;
+                kopeks = summa - r * 100;
+            }
+            else
+            {
+                kopeks = summa;
+            }
+        }
+
+        public string Print()
+        {
+            return (rubles > 0 ? rubles + " рублей " : string.Empty) + (kopeks > 0 ? kopeks + " копеек" : string.Empty);
+        }
+
+    }
+}
+
+
+
---
+++ 04.09.21 Классы и перегрузка/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04._09._21_Классы_и_перегрузка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m = new Money();
+            while (true)
+            {                
+                Console.WriteLine("Введите копейки для добавления");
+                string buf = Console.ReadLine();
+                int n;
+                if (int.TryParse(buf, out n))
+                {
+                    m.AddKopeks(n);
+                    Console.WriteLine(m.Print());
+                } else
+                {
+                    Console.WriteLine("Ошибка ввода данных");
+                }
+            }
+        }
+    }
+}
